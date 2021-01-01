1f2b25e0ab47ba3100ff8d2648d9fe783281514e
Shubidumdu <skaty4870@naver.com>
2021-01-01 19:09:06 +0900

    Prevent scroll-refresh on mobile

--- src/pages/portfolio/components/layout/Content.js
+++ src/pages/portfolio/components/layout/Content.js
@@ -3,8 +3,12 @@ import { Layout } from 'antd';
 
 const { Content: C } = Layout;
 
+const StyledContent = styled(C)`
+  overscroll-behavior-y: contain;
+`;
+
 const Content = ({ children }) => {
-  return <C>{children}</C>;
+  return <StyledContent>{children}</StyledContent>;
 };
 
 export default Content;
